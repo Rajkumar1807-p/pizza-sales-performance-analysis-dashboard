bcdcf2e80f810d1b6e064c48f0da456f14cc0138
Rajkumar1807-p <rajkumarpothunuri@gmail.com>
2026-07-18 01:42:47 +0530

    Add files via upload

---
+++ PizzaSalesQueries.sql/SQLQuery1.sql
@@ -0,0 +1,69 @@
+select * from pizza_sales;
+
+select sum(total_price) from pizza_sales;
+
+select sum(total_price)/count(distinct order_id) from pizza_sales;
+
+select sum(quantity) from pizza_sales;
+
+select count(distinct order_id) from pizza_sales;
+
+select cast(cast(sum(quantity) as decimal(10,2))/cast(count(distinct order_id) as decimal(10,2)) as decimal(10,2)) from pizza_sales;
+
+---daily trend
+SELECT DATENAME(DW, order_date) AS order_day, COUNT(DISTINCT order_id) AS total_orders 
+FROM pizza_sales
+GROUP BY DATENAME(DW, order_date)
+
+--hourly trend
+SELECT DATEPART(HOUR, order_time) as order_hours, COUNT(DISTINCT order_id) as total_orders
+from pizza_sales
+group by DATEPART(HOUR, order_time)
+order by DATEPART(HOUR, order_time)
+
+
+--% of Sales by Pizza Category
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_category
+
+-- % of Sales by Pizza Size
+SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+GROUP BY pizza_size
+ORDER BY pizza_size
+
+--for the first quarter
+SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales where datepart(quarter,order_date)=1
+) AS DECIMAL(10,2)) AS PCT
+FROM pizza_sales
+where datepart(quarter,order_date)=1
+GROUP BY pizza_size
+ORDER BY pizza_size
+
+--Total Pizzas Sold by Pizza Category
+SELECT pizza_category, SUM(quantity) as Total_Quantity_Sold
+FROM pizza_sales
+WHERE MONTH(order_date) = 2
+GROUP BY pizza_category
+ORDER BY Total_Quantity_Sold DESC
+
+
+-- Top 5 Best Sellers by Total Pizzas Sold
+SELECT Top 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC
+
+
+--Bottom 5 Best Sellers by Total Pizzas Sold
+SELECT TOP 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold ASC
+
+
+
